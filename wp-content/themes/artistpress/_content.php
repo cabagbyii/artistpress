8fac91b3cf18e781d43df502e4da6dd5b8aa6a6b
Calvin <cabagbyii@gmail.com>
2018-10-02 12:38:54 -0400

    added functions.php to theme; enqueued all scripts, stylesheets, and fonts; added dashboard page for setting social media links for later; started on special post templates

--- wp-content/themes/artistpress/_content.php
+++ wp-content/themes/artistpress/_content.php
@@ -2,7 +2,7 @@
 	<div class="container section-inner">
 		<div class="inner_container">
 			<div class="embed-responsive embed-responsive-16by9">
-			  	<iframe class="embed-responsive-item" src="//www.youtube.com/embed/videoseries?list=PLVbKbobB_JqBSYcUwLizOaFaB0A83Z2DX&autoplay=0" allowfullscreen></iframe>
+			  	<?php the_content(); ?>
 			</div>
 		</div>
 	</div>
--- wp-content/themes/artistpress/footer.php
+++ wp-content/themes/artistpress/footer.php
@@ -1,8 +1,4 @@
 	</div> <!-- #home-head -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="http://static.tumblr.com/voy8cl3/8goon9v23/bootstrap.min.js"></script>
-	<script type="text/javascript" src='<?php echo get_bloginfo('template_directory'); ?>/js/info.js' ></script>
-	<script type="text/javascript" src="<?php echo get_bloginfo('template_directory'); ?>/js/load.js" ></script>
 	<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us15.list-manage.com","uuid":"c7e1763dface50beb1bcf4c21","lid":"ca92707eb9"}) })</script>
 	<script type="text/javascript">
 		/*(function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
