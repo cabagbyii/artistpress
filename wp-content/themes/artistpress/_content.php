e4bbba7a89b809c164660feb78dcf910623f710e
Calvin <cabagbyii@gmail.com>
2018-10-04 12:46:04 -0400

    deleted excess files

--- wp-content/themes/artistpress/_content.php
+++
@@ -1,9 +0,0 @@
-<div class="page-section video_playlist">
-	<div class="container section-inner">
-		<div class="inner_container">
-			<div class="embed-responsive embed-responsive-16by9">
-			  	<?php the_content(); ?>
-			</div>
-		</div>
-	</div>
-</div>
